dcd9884f212aebaed128c2a1299136f17cb4bede
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-01-22 17:48:59 +0200

    Refactoring

--- workspace/Angr/src/fi/hbp/angr/logic/ModelContactListener.java
+++ workspace/Angr/src/fi/hbp/angr/logic/ModelContactListener.java
@@ -6,10 +6,19 @@ import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
+import fi.hbp.angr.ItemDestruction;
 import fi.hbp.angr.models.Destructible;
 
 public class ModelContactListener implements ContactListener
 {
+    private ItemDestruction itdes;
+    private ScoreCounter score;
+
+    public ModelContactListener(ItemDestruction itdes, ScoreCounter score) {
+        this.itdes = itdes;
+        this.score = score;
+    }
+
     @Override
     public void beginContact(Contact arg0) {
     }
@@ -22,31 +31,46 @@ public class ModelContactListener implements ContactListener
     public void postSolve(Contact contact, ContactImpulse impulse) {
         if(impulse.getNormalImpulses()[0] > 10)
         {
-            // TODO don't damage if using slingshot?
-
-            /* If A is the body that was hit */
+            /* If body A was hit */
             if(contact.getFixtureA() != null)
             {
                 if (Actor.class.isInstance(contact.getFixtureA().getBody().getUserData())) {
                     Actor a = (Actor) contact.getFixtureA().getBody().getUserData();
-                    if (Destructible.class.isInstance(a)) {
-                        ((Destructible)a).getDatamageModel().hit(impulse.getNormalImpulses()[0]);
-                    }
+                    evalDamage(a, impulse);
                 }
             }
 
-            /* If B is the body that was hit */
+            /* If body B was hit */
             if(contact.getFixtureB() != null)
             {
                 if (Actor.class.isInstance(contact.getFixtureB().getBody().getUserData())) {
-                    Actor a = (Actor) contact.getFixtureB().getBody().getUserData();
-                    if (Destructible.class.isInstance(a)) {
-                        ((Destructible)a).getDatamageModel().hit(impulse.getNormalImpulses()[0]);
-                    }
+                    Actor b = (Actor) contact.getFixtureB().getBody().getUserData();
+                    evalDamage(b, impulse);
                 }
             }
         }
+    }
 
+    /**
+     * Evaluate damage and score, and check if actor needs to be destroyed
+     * @param actor Current actor
+     * @param impulse Impulse
+     */
+    private void evalDamage(Actor actor, ContactImpulse impulse) {
+        if (Destructible.class.isInstance(actor)) {
+            /* Hit and check final health status */
+            ((Destructible)actor).getDatamageModel().hit(impulse.getNormalImpulses()[0]);
+            if (((Destructible)actor).getDatamageModel().getHealth() < 0.01f) {
+
+                if (!itdes.contains(actor)) {
+                    /* Update score counter */
+                    score.addPoints(((Destructible)actor).getDatamageModel().getPoints());
+
+                    /* Destroy actor */
+                    itdes.add(actor);
+                }
+            }
+        }
     }
 
     @Override
